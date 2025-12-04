bb5d910deab2771e519e2a9089cbc48a91062dd2
makwana hitesh <makwanah538@gmail.com>
2025-12-04 18:28:23 +0530

    sg group add

--- networking/main.tf
+++ networking/main.tf
@@ -108,3 +108,38 @@ resource "aws_route_table_association" "private" {
   subnet_id      = each.value
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_security_group" "name" {
+  name        = "my_custom_sg"
+  description = "Custom security group"
+  vpc_id      = aws_vpc.name.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "my_custom_sg"
+  }
+}
